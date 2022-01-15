2ffaaad55f1202f7f563507ff4bb9cf8a9d32f9a
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2022-01-15 16:32:33 -0300

    update

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -7,7 +7,7 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
   function handleAddNewTask() {
     //TODO - Call addTask if task not empty and clean input value 
@@ -21,13 +21,13 @@ export function TodoInput({ addTask }: TodoInputProps) {
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
-        //TODO - use value, onChangeText and onSubmitEditing props
+        onChangeText={setTask}
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
-        //TODO - onPress prop
+        onPress={handleAddNewTask}
       >
         <Icon name="chevron-right" size={24} color="#B2B2B2" />
       </TouchableOpacity>
